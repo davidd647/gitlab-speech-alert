abfa19d8d2de754a08d03d8d67be6475a6544a85
David Dales <david.dales@carpages.ca>
2018-05-10 09:41:23 -0400

    Simplified notification words and console logs (less annoying)

--- content.js
+++ content.js
@@ -2,7 +2,8 @@ console.log('🔥Thank you🔥 for running the GitLab Finn Notify script! The an
 
 var projectName = document.querySelector('.sidebar-context-title').innerText;
 
-console.log('%c       \n      ', 'font-size: 100px; background: url(https://s8.favim.com/orig/141226/adventure-time-cartoon-finn-gif-Favim.com-2337968.gif) no-repeat;');
+console.log('gitlab-speech-tracker is on');
+// console.log('%c       \n      ', 'font-size: 100px; background: url(https://s8.favim.com/orig/141226/adventure-time-cartoon-finn-gif-Favim.com-2337968.gif) no-repeat;');
 
 var tracking = document.querySelector('.ci-running') ? true : false;
 var contentWrapper = document.querySelector('.content-wrapper');
@@ -19,7 +20,7 @@ var contentObserver = new MutationObserver(function(mutationsList){
     var voices = speechSynthesis.getVoices();
     var msg = new SpeechSynthesisUtterance();
     msg.text =
-      'Good day. This is your computer notifying you that ' +
+      'Notification: ' +
       projectName +
       ' has finished pushing';
     msg.rate = 1.1;
@@ -36,7 +37,7 @@ var contentObserver = new MutationObserver(function(mutationsList){
     var voices = speechSynthesis.getVoices();
     var msg = new SpeechSynthesisUtterance();
     msg.text =
-      'Good day. A pipeline process for ' + projectName + ' is now being tracked.';
+      'Notification: a pipeline for ' + projectName + ' is now being tracked.';
     msg.rate = 1.1;
     msg.pitch = 0.5;
     window.speechSynthesis.speak(msg);
